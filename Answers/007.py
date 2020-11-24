7e80fc32a859622b345aef355f9919e56c29ec00
Taso <66312284+Siphilion@users.noreply.github.com>
2020-11-24 17:05:11 +0400

    Add files via upload

---
+++ Answers/007.py
@@ -0,0 +1,39 @@
+"""Birthday"""
+# 007.py
+# Ask for users name, and age, cur.age + 1 = next age.
+
+
+# Defining Functions
+def primaryname():
+    """Forename function; returns forename if forename is alphabetic"""
+    while True:
+        name_1 = input('Please enter your firstname: ')
+        if not name_1.isalpha():
+            print('Your firstname cannot contain anything other than alphabets.')
+        else:
+            break
+
+
+def addage():
+    """Adds age from current age."""
+    while True:
+        askage = input('Please enter your age: ')
+        if not askage.isnumeric():
+            print('Your age cannot be anything other than a integer.')
+        else:
+            askage = int(askage)
+            newage = askage + 1
+            print('You will be", newage, "next year!')
+            break
+
+
+def main():
+    """Driver"""
+    primaryname()
+    addage()
+
+
+# Calling Functions
+main()
+
+# End
