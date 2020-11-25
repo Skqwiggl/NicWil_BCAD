b888a52baae8f1440cd628d696f3c36469f0701a
Taso <66312284+TasMav@users.noreply.github.com>
2020-11-25 07:38:15 +0400

    Update 002.py

--- Answers/002.py
+++ Answers/002.py
@@ -1,6 +1,6 @@
 """Wholename"""
 # 002.py
-# User enters name, returns name if script satisfied.
+# Return whole name of user.
 
 # Importing Modules
 import time
@@ -11,22 +11,22 @@ import time
 def wholename():
     """Returns name when function is satisfied."""
     while True:
-        name_1 = input("Please enter your firstname: ")
+        name_1 = input('Please enter your firstname: ')
         if not name_1.isalpha():
-            print("Your firstname cannot be alphanumeric/blank, please retry.")
+            print('Your forename cannot contain anything other than alphabets..')
         else:
-            print("Processing...")
+            print('Processing...')
             time.sleep(2)
             break
 
     while True:
-        name_2 = input("Now enter your surname: ")
+        name_2 = input('Now enter your surname: ')
         if not name_2.isalpha():
-            print("Your surname cannot be alphanumeric/blank, please try again!")
+            print('Your surname cannot contain anything other than alphabets.')
         else:
-            print("Initialising...")
+            print('Initialising...')
             time.sleep(2)
-            print("Hello " + name_1 + " " + name_2 + "!")
+            print('Hello ' + name_1 + ' ' + name_2 + '!')
             break
 
 
