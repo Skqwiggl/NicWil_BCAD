6d1e9942b49b78e7eb5a19a7ef945b2873c31afc
Taso <66312284+TasMav@users.noreply.github.com>
2020-11-26 18:31:13 +0400

    Update 004.py
    Altered the contents within the function.

--- Answers/004.py
+++ Answers/004.py
@@ -6,28 +6,30 @@
 # Start
 # Defining Functions
 def addition():
-    """Finds the sum of a + b."""
+    """Finds the sum; c."""
     while True:
         a = input('Please enter a value for a: ')
-        if '.' in a:
-            a = float(a)
-            break
-        elif not a.isnumeric():
-            print('Your value for a cannot be anything but a number, please retry.')
-        else:
-            a = int(a)
-            break
+        try:
+            if '.' in a:
+                a = float(a)
+                break
+            elif '.' not in a:
+                a = int(a)
+                break
+        except ValueError:
+            print('Your value for a cannot be anything but a number, please')
 
     while True:
         b = input('Please enter a value for b: ')
-        if '.' in b:
-            b = float(b)
-            break
-        elif not b.isnumeric():
-            print('Your value for b cannot be anything but a number, please try again.')
-        else:
-            b = int(b)
-            break
+        try:
+            if '.' in b:
+                b = float(b)
+                break
+            elif '.' not in b:
+                b = int(b)
+                break
+        except ValueError:
+            print('Your value for b cannot be anything but a number, please')
 
     c = a + b
     print(a, '+', b, '=', c)
