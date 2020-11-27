0dfc4bced20d187a843d7686b19274b934509ed3
Taso <66312284+TasMav@users.noreply.github.com>
2020-11-27 17:20:33 +0400

    Update 005.py
    Copied func addition() from 004.py and added it here, and expanded it to satisfy the question for 005.py

--- Answers/005.py
+++ Answers/005.py
@@ -1,32 +1,51 @@
 """Addition"""
+# 005.py
 # User enters 2 numbers, find sum & display.
 
 
+# Start
+# Defining Functions
 def addition():
     """Finds the sum; c."""
     while True:
-        a = input("Please enter a number for the value for a: ")
-        if not a.isnumeric():
-            print("Your value for a cannot be alphanumeric/blank, please retry.")
-        else:
-            break
+        a = input('Please enter a value for a: ')
+        try:
+            if '.' in a:
+                a = float(a)
+                break
+            elif '.' not in a:
+                a = int(a)
+                break
+        except ValueError:
+            print('Your value for a cannot be anything but a number, please')
 
     while True:
-        b = input("Now enter a value for b: ")
-        if not b.isnumeric():
-            print("Your value for b cannot be alphanumeric/blank, please re-enter.")
-        else:
-            break
+        b = input('Please enter a value for b: ')
+        try:
+            if '.' in b:
+                b = float(b)
+                break
+            elif '.' not in b:
+                b = int(b)
+                break
+        except ValueError:
+            print('Your value for b cannot be anything but a number, please')
 
     while True:
-        c = input("Enter a value for c: ")
-        if not c.isnumeric():
-            print("Your value for c cannot be alphanumeric/blank, please try again.")
-        else:
-            d = float(a) + float(b)
-            e = float(d) * float(c)
-            print("The product of the sum", d, "*", c, "=", e)
-            break
+        c = input('Please enter a value for c: ')
+        try:
+            if '.' in c:
+                c = float(c)
+                break
+            elif '.' not in c:
+                c = int(c)
+                break
+        except ValueError:
+            print('Your value for c cannot be anything but a number, please')
+
+    d = a + b
+    e = d * c
+    print("The product of the sum", d, "*", c, "=", e)
 
 
 def main():
@@ -34,4 +53,7 @@ def main():
     addition()
 
 
+# Calling Functions
 main()
+
+# End
