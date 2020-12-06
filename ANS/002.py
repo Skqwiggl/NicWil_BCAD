c8d5a573a531c35bb9edd50633bfb9e06180c1ea
Taso <66312284+TasMav@users.noreply.github.com>
2020-12-06 08:37:27 +0400

    Update 002.py
    Reduced size of code, and altered it.

--- ANS/002.py
+++ ANS/002.py
@@ -1,41 +1,29 @@
 """Wholename"""
-# 002.py
-# Return whole name of user.
+# Ask for the users' name and display it.
 
-# Importing Modules
 import time
 
 
-# Start
-# Defining Functions
 def wholename():
     """Returns name when function is satisfied."""
     while True:
-        name_1 = input('Please enter your firstname: ')
-        if not name_1.isalpha():
-            print('Your forename cannot contain anything other than alphabets..')
+        fornam = input("Enter your forename: ")
+        if not fornam.isalpha():
+            print("Your forename cannot contain anything other than alphabets.")
         else:
-            print('Processing...')
+            print("Processing...")
             time.sleep(2)
             break
 
     while True:
-        name_2 = input('Now enter your surname: ')
-        if not name_2.isalpha():
-            print('Your surname cannot contain anything other than alphabets.')
+        surnam = input("Enter your surname: ")
+        if not surnam.isalpha():
+            print("Your surname cannot contain anything other than alphabets.")
         else:
             print('Initialising...')
             time.sleep(2)
-            print('Hello ' + name_1 + ' ' + name_2 + '!')
+            print("Hello " + fornam + " " + surnam + "!")
             break
 
 
-def main():
-    """Driver"""
-    wholename()
-
-
-# Calling Functions
-main()
-
-# End
+wholename()
