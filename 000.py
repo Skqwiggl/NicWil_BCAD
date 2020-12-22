ae6a04621cea5516810068fa0d9cff1a7c2c7a0a
Taso <66312284+TasMav@users.noreply.github.com>
2020-12-22 13:14:04 +0400

    Add files via upload

---
+++ 000.py
@@ -0,0 +1,20 @@
+"""Forename"""
+# Ask for the users' name then display it.
+
+import time
+
+
+def forename():
+    """Return primary name."""
+    while True:
+        fornam = input("Enter your forename: ")
+        if not fornam.isalpha():
+            print("Your firstname cannot contain anything other than alphabets.")
+        else:
+            print("Processing...")
+            time.sleep(2)
+            print("Hello " + fornam + "!")
+            break
+
+
+forename()
